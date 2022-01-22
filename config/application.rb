06c5da4c8ee01fede3cabe3b7b924f57198f5ff3
Artem <porubenko99@mail.ru>
2022-01-23 01:31:24 +0300

    Init pg database

--- config/application.rb
+++ config/application.rb
@@ -11,17 +11,10 @@ module QuizAppApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    env_file = File.join(Rails.root, 'config', 'env.yml')
+    YAML.load(File.open(env_file)).each do |key, value|
+      ENV[key.to_s] = value
+    end if File.exists?(env_file)
     config.api_only = true
   end
 end
