de4e4789dcc6ae741bdc6a2a072b055eba977c8c
Artem <porubenko99@mail.ru>
2022-01-23 01:41:27 +0300

    Add seeds data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+3.times do
+	user = User.create({name: Faker::Name.name})
+
+	4.times do
+		game = Faker::Game
+		user.questions.create({
+			content: "What is #{game.title} genre?",
+			answer: game.genre,
+		})
+	end
+end
